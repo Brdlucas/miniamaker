20ce2e24d2069ff4cdd966bbd9dc9af2256bf341
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-04 14:22:07 +0100

    Désactivation temporaire de l'appel à la méthode de connexion dans le contrôleur d'inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -58,7 +58,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $security->login($user, 'form_login', 'main');
+            // return $security->login($user, 'form_login', 'main');
         }
 
         return $this->render('registration/register.html.twig', [
