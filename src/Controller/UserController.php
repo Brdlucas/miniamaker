90755429930bd0e385cc5493891ec40f61cf392e
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-05 16:56:02 +0100

    ajout de la logique de complétion du profil utilisateur avec validation des champs et gestion des messages flash

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,11 +18,27 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/complete', name: 'app_complete', methods: ['GET', 'POST'])]
-    public function complete(): Response
+    #[Route('/complete', name: 'app_complete', methods: ['POST'])]
+    public function complete(Request $request, EntityManagerInterface $em): Response
     {
-        //    Redirection avec flash message
-        $this->addFlash('success', 'Votre profil est complété');
-        return $this->render('app_profile');
+        $data = $request->getPayload();
+
+        if (!empty($data->get('username')) && !empty($data->get('fullname'))) {
+            $user = $this->getUser();
+
+            $user
+                ->setUsername($data->get('username'))
+                ->setFullname($data->get('fullname'))
+            ;
+
+            $em->persist($user);
+            $em->flush();
+
+            //    Redirection avec flash message
+            $this->addFlash('success', 'Votre profil est complété');
+        } else {
+            $this->addFlash('error', 'Vous devez remplir tous les champs');
+        }
+        return $this->redirectToRoute('app_profile');
     }
 }
