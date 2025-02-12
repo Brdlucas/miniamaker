229a1fda4b533a1f4f747b8d7b69741ed638a005
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-12 11:30:35 +0100

    ajout de la gestion des paiements d'abonnement avec création de session Stripe et gestion des erreurs

--- src/service/PaymentService.php
+++ src/service/PaymentService.php
@@ -2,38 +2,74 @@
 
 namespace App\Service;
 
-use App\Entity\Subscription;
-use App\Entity\User;
-use App\Repository\SubscriptionRepository;
+
 use Stripe\Stripe;
-use App\Service\AbstractService;
+use App\Entity\User;
+use App\Entity\Subscription;
+use Stripe\Checkout\Session;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\SubscriptionRepository;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
-/*
- * Classe PaymentService dédiée à la gestion du paiement des abonnements des utilisateurs
- */
-
-class PaymentService extends AbstractService
+class PaymentService
 {
-    public function __construct(private ParameterBagInterface $params, private EntityManagerInterface $em, private SubscriptionRepository $sr)
-    {
-        $this->params = $params;
-        $this->sr = $sr;
-        $this->em = $em;
-    }
+    public function __construct(
+        private ParameterBagInterface $params,
+        private SubscriptionRepository $sr,
+        private EntityManagerInterface $em,
+        private HttpClientInterface $httpClient,
+    ) {}
 
-    public function setPayment(User $user, int $amount): void
+    public function setPayment(User $user, int $amount): string
     {
         Stripe::setApiKey($this->params->get('STRIPE_SK'));
 
-        $subscription = new Subscription();
-        $subscription
-            ->setClient($user)
-            ->setAmount($amount)
-            ->setFrequency($amount > 99 ? $this->params->get('STRIPE_SUB_ANNUALLY') : $this->params->get('STRIPE_SUB_MONTHLY'))
-        ;
 
-        dd($subscription);
+        try {
+            $subscription = $user->getSubscription();
+
+            if ($user->getSubscription() === null) {
+                $subscription = new Subscription();
+                $subscription->setClient($user);
+            }
+
+            $subscription
+                ->setAmount($amount)
+                ->setFrequency($amount > 99 ? 'year' : 'month')
+            ;
+
+            $this->em->persist($subscription);
+            $this->em->flush();
+
+            $checkout_session = Session::create([
+                'payment_method_types' => ['card'], // Setup du moyen de paiement
+                'line_items' => [[
+                    'price_data' => [
+                        'currency' => 'eur', // Setup de la devise
+                        'unit_amount' => $amount * 100, // Montant en centimes
+                        'recurring' => [ // Recurrence de l'abonnement
+                            'interval' => $subscription->getFrequency(), // mois ou année
+                        ],
+                        'product_data' => [ // Informations du produit
+                            'name' => 'miniamaker', // Texte affiché sur la page de paiement
+                        ],
+                    ],
+                    'quantity' => 1, // Qt obligatoire
+                ]],
+                'mode' => 'subscription', // Mode de paiement
+                // Redireection après le paiement (réussi ou échoué)
+                'success_url' => $this->params->get('APP_URL') . '/subscription/success?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => $this->params->get('APP_URL') . '/subscription/cancel',
+            ]);
+
+            if (!isset($checkout_session->url)) {
+                throw new \Exception('Erreur lors de la création de la session de paiement');
+            }
+
+            return $checkout_session->url; // Le service retourne une URL au controleur
+        } catch (\Throwable $th) {
+            throw new \RuntimeException('Erreur lors de la création de la session de paiement : ' . $th->getMessage());
+        }
     }
 }
