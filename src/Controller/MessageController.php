b5225c55c70cb0070b329c8e99d9584c61fb5b3f
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-12 16:59:49 +0100

    ajout de la gestion des messages avec création et affichage dans le contrôleur MessageController

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Repository\DiscussionRepository;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
 final class MessageController extends AbstractController
 {
-    #[Route('/message', name: 'app_message')]
+    public function __construct(
+        private DiscussionRepository $dr,
+        private MessageRepository $mr,
+        private EntityManagerInterface $em
+    ) {}
+
+    #[Route('/messages', name: 'app_message', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+        return $this->render('message/index.html.twig', []);
+    }
+
+
+    #[Route('/messages/{id}', name: 'app_message_show', methods: ['GET', 'POST'])]
+    public function show($id, Request $request,): Response
+    {
+        if ($request->isMethod('POST')) {
+            $mes = new Message();
+            $mes
+                ->setDiscussion($this->dr->find($id))
+                ->setUser($this->getUser())
+                ->setContent($request->get('message'))
+                ->setStatus(true)
+            ;
+            $this->em->persist($mes);
+            $this->em->flush();
+
+            if ($request->headers->get('HX-Request')) {
+                return $this->render('message/_message.html.twig', [
+                    'item' => $mes
+                ]);
+            }
+        };
+        return $this->render('message/show.html.twig', [
+            'messages' => $this->mr->findByDiscussion($id, ['created_at' => 'DESC']),
         ]);
     }
 }
